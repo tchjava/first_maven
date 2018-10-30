a114eaa1a9d6d80368e65d64985588e82d5bd07b
tchjava <1310120194@qq.com>
2018-10-30 22:46:39 +0800

    add method of sun

--- src/main/java/com/weng/test/Demo.java
+++ src/main/java/com/weng/test/Demo.java
@@ -1,28 +1,7 @@
 package com.weng.test;
 
 public class Demo {
-    public static void main(String[] args) {
-        System.out.println("你好吗");
-    }
-    public void fun(){
-        System.out.print("Hello");
-         System.out.print("He2llo");
-    }
-
-
-    public void fun1(){
-
-    }
-
-    public void fun2(){
-         System.out.print("fun2...");
-    }
-    public void fun3()
-    {
-        System.out.println("你好");
-    }
-
-    public void fun6(){
-        System.out.println("开发了6");
+    public void sun(){
+        System.out.println("sun..");
     }
 }
